2db3bf9e705835dd897071516df6abe7b938e8cf
K218400479A <88258831+K218400479A@users.noreply.github.com>
2022-11-29 01:34:45 +0000

    add basename to BrowserRouter, homepage to package.json, created .env.sample

--- src/index.js
+++ src/index.js
@@ -2,10 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
-
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.REACT_APP_PREPEND_URI}>
     <App />
     </BrowserRouter>
   </React.StrictMode>,
