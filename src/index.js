9e70962fc32358666ba7d347c8b2d8c70814e4ea
K218400479A <88258831+K218400479A@users.noreply.github.com>
2023-01-01 00:57:47 +0000

    dynamic basename for BrowserRouter, /build in gitignore, created env.sample

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.REACT_APP_PREPEND_URI}>
     <App />
     </BrowserRouter>
   </React.StrictMode>,
